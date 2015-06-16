9bcfd83eabf28975ecc5fb93fd88e09adb774bf7
Mihail Georgiev <mihail.georgiev@wooga.net>
2015-06-16 16:30:50 +0200

    cleanup

--- Assets/Libraries/Entitas.Unity.CodeGenerator/Editor/CodeGeneratorEditor.cs
+++ Assets/Libraries/Entitas.Unity.CodeGenerator/Editor/CodeGeneratorEditor.cs
@@ -18,31 +18,5 @@ namespace Entitas.Unity.CodeGenerator {
             Entitas.CodeGenerator.CodeGenerator.Generate(types, config.pools, config.generatedFolderPath);
             AssetDatabase.Refresh();
         }
-		//I did this :)
-		[MenuItem("Entitas/Test")]
-		public static void Test() {
-			String[] lines = File.ReadAllLines("Assets/Logs/2015-05-20_TestLog(7).txt");
-			Dictionary<String, List<String>> entityEntries = new Dictionary<String, List<String>>();
-
-			foreach (String line in lines)
-			{
-				String[] split = line.Split(':');
-
-				if (!entityEntries.ContainsKey(split[0]))
-				{
-					entityEntries.Add(split[0], new List<string>());
-				}
-				if(split.Length>1)
-					entityEntries[split[0]].Add(split[1]);
-
-			}
-
-			foreach (KeyValuePair<String, List<string>> kvp in entityEntries)
-			{
-				Debug.Log(kvp.Key);
-				Debug.Log("\t" + string.Join("\n\t", kvp.Value.ToArray()));
-
-			}
-		}
 	}
 }
